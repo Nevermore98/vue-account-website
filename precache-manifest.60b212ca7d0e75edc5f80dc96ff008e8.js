7ba92141568ad6ff55315e8ea224541ec1d862d7
Nevermore98 <nevermorey98@qq.com>
2021-10-11 21:30:06 +0800

    deploy

---
+++ precache-manifest.60b212ca7d0e75edc5f80dc96ff008e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b9116f3adb3831cf278",
+    "url": "/vue-account-website/css/app.40419bd1.css"
+  },
+  {
+    "revision": "dc88da06419553294d39a36d5f39802c",
+    "url": "/vue-account-website/index.html"
+  },
+  {
+    "revision": "5b9116f3adb3831cf278",
+    "url": "/vue-account-website/js/app.59ed25c9.js"
+  },
+  {
+    "revision": "c968665f4c854648c081",
+    "url": "/vue-account-website/js/chunk-vendors.dee4dd9c.js"
+  },
+  {
+    "revision": "852fdb4ffb1dbaaf5c12eebd3902c8d1",
+    "url": "/vue-account-website/manifest.json"
+  },
+  {
+    "revision": "62066db5da3a55138c87c9857f88efa6",
+    "url": "/vue-account-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-account-website/robots.txt"
+  }
+]);
